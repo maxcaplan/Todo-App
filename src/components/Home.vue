64f1d27fdddae8a71981c56c4073de55ae50af99
jellycream <maxacaplan@gmail.com>
2018-12-29 13:02:54 -0400

    Added realtime clock

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container">
     <h1>Home</h1>
+    <h2>{{currentTime}}</h2>
     <!-- <button class="btn btn-secondary" @click="makeData">Make</button> -->
     <hr>
     <!-- <input
@@ -88,11 +89,13 @@
 </template>
 <script>
 import db from "../firebase/firebaseInit.js";
+import moment from 'moment'
 var todosCollection = db.collection("todos");
 
 export default {
   data() {
     return {
+      currentTime: null,
       test: [],
       loading: false,
       newTodo: "",
@@ -128,6 +131,9 @@ export default {
         this.loading = true;
       }
     });
+
+    this.currentTime = moment().format("LTS");
+    setInterval(() => this.updateCurrentTime(), 1 * 1000);
   },
 
   filters: {
@@ -172,6 +178,11 @@ export default {
     },
     doneTestEdit(todo) {
       todo.editing = false;
+    },
+
+    //clock script
+    updateCurrentTime() {
+      this.currentTime = moment().format('LTS');
     },
 
     //todo logic
